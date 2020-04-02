bef1f1323f50c097129b6ee0ebd0fbc234310abe
berloo <example@mail.com>
2020-04-02 19:02:05 +0600

    first

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-/*let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+'use strict';
 
-let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: false
-};
+// let money = prompt("Ваш бюджет на месяц?", 'Например 100тг'),
+// 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+// let appData = {
+// 	budget: money,
+// 	expenses: {},
+// 	optionalExpenses: {},
+// 	income: [],
+// 	timeData: time,
+// 	savings: false
+// };
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+// let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 	a2 = prompt("Во сколько обойдется?", ''),
+// 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 	a4 = prompt("Во сколько обойдется?", '');
 
-alert(appData.budget / 30);*/
+// appData.expenses.a1 = a2;
+// appData.expenses.a3 = a4;
+
+// alert(appData.budget / 30);
+
+
+/*
 let num = 0;
-
 if (num < 49) {
 	console.log('неверно!')
 } else if (num > 100) {
@@ -47,4 +51,119 @@ switch (num) {
 	default :
 		console.log('Что то пошло не так!');
 		break;
+}
+
+let num = 50;
+
+// while (num < 55) {
+// 	console.log(num);
+// 	num++;
+// }
+
+do {
+	console.log(num);
+	num++;
+}
+while(num < 55);
+
+for (let i = 1; i < 8; i++){
+	if (i==6){
+		continue
+	}
+	
+	console.log(i)
+}*/
+
+'use strict';
+
+
+let money = +prompt ("Ваш бюджет на месяц?", ""),
+    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+    
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt ("Во сколько обойдется?", "");
+
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+        console.log ("done");
+
+        appData.expenses[a] = b;
+    } else {                            
+        console.log ("bad result");
+        i--;
+    }
+
+};
+
+
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Это минимальный уровень достатка!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Это средний уровень достатка!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Это высокий уровень достатка!");
+} else {
+    console.log ("Произошла ошибка");
 }
