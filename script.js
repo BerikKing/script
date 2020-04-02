cea776c2e73999132cb2469da293c59466817824
berloo <example@mail.com>
2020-04-02 17:22:23 +0600

    третый

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+/*let money = prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -18,4 +18,33 @@ let a1 = prompt("Введите обязательную статью расхо
 appData.expenses.a1 = a2;
 appData.expenses.a3 = a4;
 
-alert(appData.budget / 30);
+alert(appData.budget / 30);*/
+let num = 0;
+
+if (num < 49) {
+	console.log('неверно!')
+} else if (num > 100) {
+	console.log('много!')
+} else  {
+	console.log('верно!')
+};
+
+(num == 100) ? console.log('верно!') :  console.log('неверно!');
+
+switch (num) {
+	case num < 49:
+	 	console.log('неверно');
+	 	break;
+	case num > 100:
+		console.log('много!');
+		break;
+	case num > 80:
+		console.log('Еще многовато!');
+		break;
+	case   50:
+		console.log('верно!');
+		break;
+	default :
+		console.log('Что то пошло не так!');
+		break;
+}
